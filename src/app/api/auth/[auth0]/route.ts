79c32f6a39ec881ac26fa499c99c568469683502
Anthony Chiavelli <anthony.chiavelli@updater.com>
2026-01-26 00:13:21 -0500

    Add temp logging

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -4,7 +4,9 @@ import { NextRequest } from "next/server";
 
 export const GET = handleAuth({
   onError: (req: NextRequest, error: HandlerError) => {
+    console.error("🌵🌵🌵🌵🌵🌵🌵🌵🌵🌵🌵🌵");
     console.error(error);
+    console.error("🌵🌵🌵🌵🌵🌵🌵🌵🌵🌵🌵🌵");
     return redirect(`http://${req.nextUrl.host}/login-error`);
   },
 });
